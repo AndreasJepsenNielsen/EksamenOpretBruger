fea9076fc9b9a7a2b47df39310cd5b4e438d6acc
lukasnielsen98 <31307764+lukasnielsen98@users.noreply.github.com>
2017-11-22 11:26:08 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Velkommen til Delfinen, vælg hvad du vil gøre ud fra de valgmuligheder i menuen");
         MedlemsListe MedlemListe = new MedlemsListe();
-        
+     
+        System.out.println("Velkommen til Delfinen, vælg hvad du vil gøre ud fra de valgmuligheder i menuen");
+      
         System.out.println("Menu:\n
                            1. Opret bruger");
                           
