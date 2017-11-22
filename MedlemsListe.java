9084eea6a48ae832f5021a9868ce1e6ab0f16acd
HardCry <aske.funder@gmail.com>
2017-11-22 14:27:10 +0100

    Update MedlemsListe.java
    lavet getter for ArrayList
    færdiggjort opretBruger

--- MedlemsListe.java
+++ MedlemsListe.java
@@ -1,15 +1,24 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class MedlemsListe  {
+public class MedlemsListe
+{
     private ArrayList<Medlem> medlemsListe;
 
-    public MedlemsListe(){
+    public MedlemsListe()
+    {
         this.medlemsListe = new ArrayList<>();
     }
 
 
-    public void opretBruger(Medlem medlem){
+    public void opretBruger()
+    {
+        Medlem medlem = new Medlem();
+
         this.medlemsListe.add(medlem);
+    }
+
+    public ArrayList<Medlem> getMedlemsListe() {
+        return medlemsListe;
     }
 }
