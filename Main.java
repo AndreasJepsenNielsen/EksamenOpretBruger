98ea1c6969523ffa6b197573dce469ed5ca4bc10
HardCry <aske.funder@gmail.com>
2017-11-22 14:25:51 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
-        MedlemsListe MedlemListe = new MedlemsListe();
-     
+import java.util.Scanner;
+
+public class Main
+{
+    Scanner input;
+
+    public static void main(String[] args)
+    {
+
+
+
+        MedlemsListe medlemsListe = new MedlemsListe();
+
+        medlemsListe.opretBruger();
+
+        System.out.println(medlemsListe.getMedlemsListe().get(0).getKontingentType());
+
         System.out.println("Velkommen til Delfinen, vælg hvad du vil gøre ud fra de valgmuligheder i menuen");
-      
-        System.out.println("Menu:\n
-                           1. Opret bruger");
-                          
+
+        System.out.println("Menu:\n " +
+                "1. Opret bruger");
+
+
+
+
     }
 }
