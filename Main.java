9d03ce344a12c2bf1e3730ff6f7cdcd0f2cbfa76
AndreasJepsenNielsen <32238389+AndreasJepsenNielsen@users.noreply.github.com>
2017-11-23 12:40:29 +0100

    Update Main.java
    opdateret med troels hjælp

--- Main.java
+++ Main.java
@@ -2,26 +2,40 @@ import java.util.Scanner;
 
 public class Main
 {
-    Scanner input;
-
     public static void main(String[] args)
     {
-
+        Scanner input = new Scanner(System.in);
 
 
         MedlemsListe medlemsListe = new MedlemsListe();
 
-        medlemsListe.opretBruger();
 
-        System.out.println(medlemsListe.getMedlemsListe().get(0).getKontingentType());
 
         System.out.println("Velkommen til Delfinen, vælg hvad du vil gøre ud fra de valgmuligheder i menuen");
 
-        System.out.println("Menu:\n " +
+
+
+        while(true)
+        {
+            System.out.println("Menu:\n " +
                 "1. Opret bruger");
 
+            String choice = input.next();
 
-
+            switch (choice)
+            {
+                case "1":
+                    medlemsListe.opretBruger();
+                    break;
+                case "2":
+                    System.out.println(medlemsListe.getMedlemsListe().get(0));
+                    System.out.println(medlemsListe.getMedlemsListe().get(1));
+                    break;
+                default:
+                    break;
+            }
+        }
 
     }
+
 }
