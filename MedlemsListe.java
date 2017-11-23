f0413a763c5784f8bdb661c3b54646594943c92a
AndreasJepsenNielsen <32238389+AndreasJepsenNielsen@users.noreply.github.com>
2017-11-23 12:42:18 +0100

    Update MedlemsListe.java
    Opdateret med troels hjælp

--- MedlemsListe.java
+++ MedlemsListe.java
@@ -1,24 +1,58 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class MedlemsListe
-{
+public class MedlemsListe {
     private ArrayList<Medlem> medlemsListe;
+    private Scanner input = new Scanner(System.in);
 
-    public MedlemsListe()
-    {
+    public MedlemsListe() {
         this.medlemsListe = new ArrayList<>();
     }
 
 
-    public void opretBruger()
-    {
-        Medlem medlem = new Medlem();
-
-        this.medlemsListe.add(medlem);
+    public void opretBruger() {
+        //Husk kommentarer til alt
+        this.medlemsListe.add(
+                new Medlem(
+                    indtastNavn(),
+                    indtastTelefonnummer(),
+                    indtastAdresse(),
+                    indtastEmail(),
+                    indtastAlder()
+                )
+        );
     }
+
+    //METHODS
+    public String indtastNavn() {
+        System.out.println("Navn");
+        return input.nextLine();
+    }
+
+    public String indtastTelefonnummer() {
+        System.out.println("Telefonnummer");
+        return input.nextLine();
+    }
+
+    public String indtastAdresse() {
+        System.out.println("Adresse");
+        return input.nextLine();
+    }
+
+    public String indtastEmail() {
+        System.out.println("Email");
+        return input.nextLine();
+    }
+
+    public String indtastAlder() {
+        System.out.println("Alder");
+        return input.nextLine();
+    }
+
+
 
     public ArrayList<Medlem> getMedlemsListe() {
         return medlemsListe;
     }
+
 }
